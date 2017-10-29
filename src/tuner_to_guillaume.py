342ae1048d9ade27ea5442e9027d86afaf40ee6a
Guillaume Mollard <guillaume.mollard2@gmail.com>
2017-10-29 10:15:21 +0000

    parallelized function

--- src/tuner_to_guillaume.py
+++ src/tuner_to_guillaume.py
@@ -1,11 +1,8 @@
-%matplotlib inline
 import proj1_helpers as utils
 import numpy as np
-%reload_ext autoreload
-%autoreload
 import algorithms as ML_alg
 import preprocessing_functions as prf
-
+from joblib import Parallel, delayed
 
 
 def main():
@@ -21,10 +18,16 @@ def main():
 
     degree_min = 30 * [1]
     degree_max = 30 * [1]
+    
+    Parallel(n_jobs = 2)(delayed(ML_alg.tuner_degree_lambda)(y = y_bin, x = std_x,\
+                    degree_min = degree_min, degree_max = degree_max, lambda_min = -4,\
+                    lambda_max = 0, nb_lambda = 30, k_fold=4, seed=1,\
+                    max_iters=10000, gamma=gam, cost="reg_logistic", tol=1e-3,\
+                    thresh_test_div=10, update_gamma=False) for gam in [1e-3,1e-4])
 
     
-    best_degree, best_lambda, min_rmse_te, min_rmse_tr = ML_alg.tuner_degree_lambda(y_bin, std_x,\
-                    degree_min, degree_max, lambda_min = -4,\
+    best_degree, best_lambda, min_rmse_te, min_rmse_tr = ML_alg.tuner_degree_lambda(y=y_bin, x=std_x,\
+                    degree_min = degree_min, degree_max = degree_max, lambda_min = -4,\
                     lambda_max = 0, nb_lambda = 30, k_fold=4, seed=1,\
                     max_iters=10000, gamma=0.00001, cost="reg_logistic", tol=1e-3,\
                     thresh_test_div=10, update_gamma=False)
