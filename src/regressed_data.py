a1e84f66c886be32f03b3ba54bd6692fa8726a1e
adrien <adrien.ruault@epfl.ch>
2017-10-30 10:54:19 +0100

    yolo

--- src/regressed_data.py
+++ src/regressed_data.py
@@ -1,10 +1,12 @@
+import preprocessing_functions as prf
+import numpy as np
+import algorithms as alg
 
 
-
-def data_nan_estimated_by_regression()
-    (y, x, event_ids) = utils.load_csv_data("../Data/train.csv")
-
+def data_nan_estimated_by_regression(x, y):
+    
     x_predicted = x
+    x_predicted[x[:,:] == -999.0] = 0.
     print(x_predicted.shape)
     for i in range(30):
         x_clean, y_clean = prf.get_clean_data(x_predicted,y)
@@ -14,16 +16,17 @@ def data_nan_estimated_by_regression()
         w, loss = alg.least_squares(x_i, tx_train)
 
         # get rows with NaN on i's dimension
-        x_nan_on_i = x[x[:,i] == -999.0]
+        x_nan_on_i = x[x[:,i] == 0.0]
         x_test = np.c_[x_nan_on_i[:,:i],x_nan_on_i[:,i+1:]]
         print(x_nan_on_i.shape)
         tx_test = alg.build_poly(x_test, 29*[1])
 
         y_pred = tx_test.dot(w)
-        x_predicted[x_predicted[:,i] == -999.0, i] = y_pred
+        x_predicted[x_predicted[:,i] == 0.0, i] = y_pred
         
-        # stndardisation of the matrix
-        x_predicted = (x_predicted - np.mean(x_predicted, axis=0)) / np.std(x_predicted, axis=0)
+        
+    # stndardisation of the matrix
+    x_predicted = (x_predicted - np.mean(x_predicted, axis=0)) / np.std(x_predicted, axis=0)
     
     return x_predicted
     
