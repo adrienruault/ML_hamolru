4e8edc997e73bcaf742659eb048bc0349783892b
Guillaume Mollard <guillaume.mollard2@gmail.com>
2017-12-16 23:39:26 +0100

    fix

--- project2/conv_net_guillaume/utilities.py
+++ project2/conv_net_guillaume/utilities.py
@@ -19,7 +19,7 @@ def deconv2d_relu(x, W, B, upscale_factor, name = 'undefined'):
     # Shape of the x tensor
     in_shape = tf.shape(x)
     i = 0
-    cond = tf.cond(in_shape[1] < 20, true_fn=1, false_fn=0)
+    cond = tf.cond(in_shape[1] < 20, true_fn=tf.constant(1), false_fn=tf.constant(0))
     h = ((in_shape[1] - 1) * stride) + 2 - cond #1*(in_shape[1] == 13)#(in_shape[1]%2)
     w = ((in_shape[2] - 1) * stride) + 2 - cond #1*(in_shape[2] == 13)#(in_shape[2]%2)
     new_shape = [in_shape[0], h, w, W.shape[3]]
